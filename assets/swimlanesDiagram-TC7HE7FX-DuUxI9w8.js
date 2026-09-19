2e05127fe14611e18e456a7c0f0f65f256fcb79c
Duck755 <Duck755@users.noreply.github.com>
2026-09-19 18:17:28 +0000

    deploy: ef2a13278cbec39c897051c314c13c44acfabd29

---
+++ assets/swimlanesDiagram-TC7HE7FX-DuUxI9w8.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-KWPJA3E3-RlXqPHvU.js";import{_ as a}from"./index-Bti3w9Zt.js";import"./chunk-5VM5RSS4-DSy6AWwA.js";import"./chunk-XXDRQBXY-CsrkRc50.js";import"./chunk-WEXAMYUT-DkEtEoyp.js";import"./chunk-GWA4HPMP-BbjPBPLc.js";import"./channel-DgHOLqaV.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster:not([data-color-id]) rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=r({styles:s});export{f as diagram};
